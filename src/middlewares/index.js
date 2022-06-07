0f1607ce89b44ef557dc840f80b2f92649b15071
Jerlysson <jerlyssonm@gmail.com>
2022-06-07 20:53:14 -0300

    fix: verify code length

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -13,9 +13,16 @@ const validateBarCode = (req, res, next) => {
     if (newCode.length < 44 || newCode.length > 48){
         return res.status(400).json({message: "Enter a valid code, the default is a numerical string of 44 to 48 characters."})
     }
-    output.barCode = getBarCode(newCode)
-    output.expirateDate = getDataInBillet(getBarCode(newCode))
-    output.amount = getPriceInBillet(getBarCode(newCode))
+
+    if(newCode.length == 44){
+        output.barCode = newCode 
+        output.expirateDate = getDataInBillet(newCode)
+        output.amount = getPriceInBillet(newCode)
+    }else {
+        output.barCode = getBarCode(newCode)
+        output.expirateDate = getDataInBillet(getBarCode(newCode))
+        output.amount = getPriceInBillet(getBarCode(newCode))
+    }
     req.output = output
     next();
 };
